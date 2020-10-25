f2f140f5645f8f41b07de3765395a130c4e159bd
QunCh <57080624+QunCh@users.noreply.github.com>
2020-10-25 20:07:53 +0800

    Stats with Excel Upload
    Stats with Excel Upload

---
+++ distribution simulator.py
@@ -0,0 +1,9 @@
+from scipy import stats
+import numpy as np
+import pandas as pd
+
+# Normal Distribution
+s = np.random.normal(5,5, 1000)
+df = pd.DataFrame(s)
+df.to_excel('simulator.xlsx', index = False)
+np.random.normal(``)
