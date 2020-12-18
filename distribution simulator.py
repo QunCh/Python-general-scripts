4bc7cea90f9f485e86aaf52b1c21d5c7cd29f688
QunCh <57080624+QunCh@users.noreply.github.com>
2020-12-18 16:46:13 +0800

    random simulation

--- distribution simulator.py
+++ distribution simulator.py
@@ -1,13 +1,19 @@
 from scipy import stats
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 # Normal Distribution
 s = np.random.normal(5,5, 1000)
 df = pd.DataFrame(s)
 df.to_excel('simulator.xlsx', index = False)
 
+np.array(np.random.random()
 
-
+parray = np.random.random(size = 1000000)
+test = parray/(1-parray)
+a = np.log(test)
+sns.distplot(a)
+sns.scatterplot(test, a)
 
 np.log(0.5)
