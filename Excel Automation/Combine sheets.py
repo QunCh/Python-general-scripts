d17d07a9566f50d5142a1404630478621e3ec5eb
QunCh <57080624+QunCh@users.noreply.github.com>
2020-12-30 18:04:25 +0800

    Update Combine sheets.py

--- Excel Automation/Combine sheets.py
+++ Excel Automation/Combine sheets.py
@@ -2,18 +2,18 @@ import pandas as pd
 import os
 os.chdir(r'C:\Github\Python-general-scripts\Excel Automation')
 
-df = pd.read_excel('combinesheets.xlsx', sheet_name=None, usecols='A:C')
+df = pd.read_excel('combinesheets.xlsx', sheet_name=None, usecols='A:C',header=None)
 data = pd.DataFrame()
 
 for key, val in df.items():
    data = pd.concat([data, val])
 
 data.shape
-data.columns
+data.columns = ['id','city','gdp']
 data = data.dropna(how = 'all')
 
 data = data.iloc[:,:-1]
 
-data.rename(columns = {'Unnamed: 1':'地市', 'Unnamed: 2':'GDP'}, inplace = True)
+# data.rename(columns = {'Unnamed: 1':'地市', 'Unnamed: 2':'GDP'}, inplace = True)
 
-data.to_excel('combine GDP.xlsx', index = False)
+data.to_excel('output.xlsx', index = False)
